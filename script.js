26ab5b01d25e13343445ce71fb604fa3ad61d6aa
Halim_Raimjanov <halimjonraimjanov@gmail.com>
2022-09-14 21:44:22 +0600

    update script

---
+++ script.js
@@ -0,0 +1,23 @@
+let qrcode = new QRCode("qrcode");
+
+function makeCode() {
+    let elText = document.getElementById('text');
+    if (!elText.value) {
+        alert('Input link or text');
+        elText.focus();
+        return;
+    }
+
+    qrcode.makeCode(elText.value);
+}
+makeCode();
+
+$("#text").
+  on("blur", function () {
+    makeCode();
+  }).
+  on("keydown", function (e) {
+    if (e.keyCode == 13) {
+      makeCode();
+    }
+  });
